9e2052486f3aecef1fadcd214e8c4875a6c9576c
Moises Higuerey <mhiguerey@outlook.com>
2022-06-05 18:29:57 -0600

    Create base objects

---
+++ src/main/java/so/filesystem/FileSystem.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package so.filesystem;
+
+/**
+ *
+ * @author Aozhen
+ */
+public class FileSystem {
+    public static void main(String args[]){
+        
+    }
+}
